79055f5a5d44c23c8cc37c7c1bd47106fb1d60e7
Tofulati <almtho2003@gmail.com>
2025-04-30 13:55:14 -0700

    update explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,43 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const voiceSelect = document.getElementById('voice-select');
+  const voiceImage = document.querySelector('#explore img');
+  const textToSpeak = document.getElementById('text-to-speak');
+  const speakButton = document.querySelector('button');
+
+  let voices = [];
+
+  function populateVoiceList() {
+    voices = speechSynthesis.getVoices();
+    
+    voiceSelect.innerHTML = '<option value="select" disabled selected>Select Voice:</option>';
+    voices.forEach(voice => {
+      const option = document.createElement('option');
+      option.textContent = `${voice.name} (${voice.lang})`;
+      option.value = voice.name;
+      voiceSelect.appendChild(option);
+    });
+  }
+
+  populateVoiceList();
+
+  speakButton.addEventListener('click', () => {
+    const selectedVoiceName = voiceSelect.value;
+    const voice = voices.find(v => v.name === selectedVoiceName);
+
+    const utterance = new SpeechSynthesisUtterance(textToSpeak.value);
+    utterance.voice = voice;
+
+    utterance.onstart = () => {
+      voiceImage.src = 'assets/images/smiling-open.png';
+    };
+
+    utterance.onend = () => {
+      voiceImage.src = 'assets/images/smiling.png';
+    };
+
+    speechSynthesis.cancel();
+    speechSynthesis.speak(utterance);
+  });
 }
