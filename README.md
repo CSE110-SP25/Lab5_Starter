491d7fc08d1717b747b59be2f64c5fb77b19115b
Tofulati <almtho2003@gmail.com>
2025-04-30 13:20:45 -0700

    update horn explore

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Lab 5 - Starter
+Names: Albert Ho, Charlie Zhu`
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,60 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const hornSelect = document.getElementById('horn-select');
+  const hornImage = document.querySelector('#expose img');
+  const audio = document.querySelector('audio');
+  const volumeSlider = document.getElementById('volume');
+  const volumeIcon = document.querySelector('#volume-controls img');
+  const playButton = document.querySelector('button');
+  const jsConfetti = new JSConfetti();
+
+  const hornData = {
+    'air-horn': {
+      image: 'assets/images/air-horn.svg',
+      sound: 'assets/audio/air-horn.mp3',
+    },
+    'car-horn': {
+      image: 'assets/images/car-horn.svg',
+      sound: 'assets/audio/car-horn.mp3',
+    },
+    'party-horn': {
+      image: 'assets/images/party-horn.svg',
+      sound: 'assets/audio/party-horn.mp3',
+    },
+  };
+
+  hornSelect.addEventListener('change', () => {
+    const selectedHorn = hornSelect.value;
+    hornImage.src = hornData[selectedHorn].image;
+    audio.src = hornData[selectedHorn].sound;
+  });
+
+  volumeSlider.addEventListener('input', () => {
+    const volume = volumeSlider.value;
+    audio.volume = volume / 100;
+
+    if (volume == 0) {
+      volumeIcon.src = 'assets/icons/volume-level-0.svg';
+    } else if (volume < 33) {
+      volumeIcon.src = 'assets/icons/volume-level-1.svg';
+    } else if (volume < 67) {
+      volumeIcon.src = 'assets/icons/volume-level-2.svg';
+    } else {
+      volumeIcon.src = 'assets/icons/volume-level-3.svg';
+    }
+  });
+
+  playButton.addEventListener('click', () => {
+    audio.play();
+
+    if (hornSelect.value === 'party-horn') {
+      jsConfetti.addConfetti({
+        emojis: ['🎉', '🎊'],
+        emojiSize: 50,
+        confettiNumber: 100,
+      });
+    }
+  });
+
 }
