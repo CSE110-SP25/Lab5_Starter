938e1c57dbb63c4e4a38a5a398a9fb63c9c351b3
이재우 <kjmin99999@gmail.com>
2025-05-03 00:12:56 -0700

    testing branch: error intentionally

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Lab 5 - Starter
+Kevin Lee
+
+1) Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+
+Answer: No, I would not use a unit test for "message" feature. The reason why I won't use the unit test is that usually "message" features are not working only by one function or a file, it usually works together with complicated connection between function and files. Unit tests are meant to test small and simple code, not about the connection between the code. 
+
+2) Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not?
+
+Answer: Yes, I would use a unit test for "max message length" feature. Unlike the previous problem, "max message length" is a small, self-contained piece of logic that can be tested with a unit test. For example I can construct a unit test, checking whether the input function correctly prevents from typing beyond the max_message_length.
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,61 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const textInput = document.getElementById('text-to-speak');
+  const voiceSelect = document.getElementById('voice-select');
+  const speakButton = document.querySelector('button');
+  const imageElement = document.querySelector('img');
+
+  // SpeechSynthesis API
+  const synth = window.speechSynthesis;
+  let voices = [];
+
+  function populateVoiceList() {
+    // Bring all the voices from the library
+      voices = synth.getVoices();
+
+      // Update to the initial state
+      voiceSelect.innerHTML = '<option value="select" disabled selected>Select Voice:</option>';
+
+      voices.forEach((voice) => {
+          const option = document.createElement('option');
+          option.textContent = `${voice.name} (${voice.lang})`;
+          option.setAttribute('data-name', voice.name);
+          option.setAttribute('data-lang', voice.lang);
+          voiceSelect.appendChild(option);
+      });
+  }
+
+  populateVoiceList();
+
+  // Some browsers load voices asynchronously
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+      speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+
+  speakButton.addEventListener('click', () => {
+      // utterThis is for the user text input.
+      const utterThis = new SpeechSynthesisUtterance(textInput.value);
+      const selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+
+      for (let i = 0; i < voices.length; i++) {
+          if (voices[i].name === selectedOption) {
+              utterThis.voice = voices[i];
+              break;
+          }
+      }
+
+      // when start speaking then smiling mouth-open face
+      utterThis.onstart = () => {
+          imageElement.src = 'assets/images/smiling-open.png';
+      };
+
+      // when end speaking then back to smiling face
+      utterThis.onend = () => {
+          imageElement.src = 'assets/images/smiling.png';
+      };
+
+      // speak utterThis
+      synth.speak(utterThis);
+  });
 }
