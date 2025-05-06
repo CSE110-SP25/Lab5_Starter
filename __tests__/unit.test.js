93c3c96cec82b1c1e8d5f138cc18c871ad17f5ee
erictuannong <erictuan23@gmail.com>
2025-05-06 00:19:20 -0700

    added two true test cases for phone numbers

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,10 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+// For phone numbers
+test('Checking if (619) 870-2550 is a valid phone number', () => {
+  expect(isPhoneNumber("(619) 870-2550")).toBe(true);
+});
+test('Checking if 916-385-9344 is a valid phone number', () => {
+  expect(isPhoneNumber("916-385-9344")).toBe(true);
+});
