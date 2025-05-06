0b9a94e8c6ee8f512ff081bc148a72d048ae8657
erictuannong <erictuan23@gmail.com>
2025-05-06 00:32:45 -0700

    added trues

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -16,3 +16,12 @@ test('Checking if (619) 870-2550 is a valid phone number', () => {
 test('Checking if 916-385-9344 is a valid phone number', () => {
   expect(isPhoneNumber("916-385-9344")).toBe(true);
 });
+
+// For emails
+test('Checking if justin20@gmail.com is a valid gmail', () => {
+  expect(isEmail("justin20@gmail.com")).toBe(true);
+});
+
+test('Checking if henry@yahoo.com is a valid gmail', () => {
+  expect(isEmail("henry@yahoo.com")).toBe(true);
+});
