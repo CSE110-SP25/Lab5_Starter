752581001fef040f05bb79a760b1ebb262f0fdc9
BenMiller0 <bem002@ucsd.edu>
2025-05-06 00:00:50 -0700

    added 20 unit tests

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Partner - n/a
 
 https://benmiller0.github.io/Lab5_Starter/expose.html
 https://benmiller0.github.io/Lab5_Starter/explore.html
+
+Expose part 3:
+1. Yes you could use a unit test to test specific features of a message feature such as correct message encryption and object creations. 
+2. Yes you would use a unit test that attempts to create a message that is about 80 charaters and checks for the correct error message. 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,72 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+// isPhoneNumber
+test('ok phone number with dashes', () => {
+  expect(isPhoneNumber('123-456-7890')).toBe(true);
+});
+test('ok phone number with parentheses', () => {
+  expect(isPhoneNumber('(123) 456-7890')).toBe(true);
+});
+test('too short', () => {
+  expect(isPhoneNumber('43-45-823')).toBe(false);
+});
+test('has letters', () => {
+  expect(isPhoneNumber('abc-s56-7890')).toBe(false);
+});
+
+// isEmail
+test('valid email', () => {
+  expect(isEmail('ben@ben.com')).toBe(true);
+});
+test('valid email with underscore', () => {
+  expect(isEmail('_ben@mail.com')).toBe(true);
+});
+test('no @', () => {
+  expect(isEmail('hi.com')).toBe(false);
+});
+test('no domain', () => {
+  expect(isEmail('hi@.com')).toBe(false);
+});
+
+// isStrongPassword 
+test('valid long password', () => {
+  expect(isStrongPassword('a123456789')).toBe(true);
+});
+test('has underscore', () => {
+  expect(isStrongPassword('A_ben2')).toBe(true);
+});
+test('not long enough', () => {
+  expect(isStrongPassword('v1')).toBe(false);
+});
+test('starts with number', () => {
+  expect(isStrongPassword('1hello')).toBe(false);
+});
+
+// isDate
+test('single-digit day/month', () => {
+  expect(isDate('5/5/2000')).toBe(true);
+});
+test('double-digit day/month', () => {
+  expect(isDate('05/05/2025')).toBe(true);
+});
+test('wrong dash placement', () => {
+  expect(isDate('12-151-111')).toBe(false);
+});
+test('2 digit year', () => {
+  expect(isDate('02/15/00')).toBe(false);
+});
+
+// isHexColor tests
+test('valid 3 digits with hash', () => {
+  expect(isHexColor('#abc')).toBe(true);
+});
+test('no hash', () => {
+  expect(isHexColor('A29')).toBe(true);
+});
+test('too many characters', () => {
+  expect(isHexColor('#ABCD2932')).toBe(false);
+});
+test('invalid chars', () => {
+  expect(isHexColor('#ay@')).toBe(false);
+});
