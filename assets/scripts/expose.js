d656b8fd00ad2644a92321cf8fc589d7bc4cceea
Jredeaux1 <130107248+Jredeaux1@users.noreply.github.com>
2025-05-08 22:52:36 -0700

    Update expose.js
    Added code for function init

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,39 @@
-// expose.js
-
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const hornSelect = document.getElementById('horn-select');
+  const hornImage = document.querySelector('img[alt="No image selected"]');
+  const hornAudio = document.querySelector('audio');
+  const volumeSlider = document.getElementById('volume');
+  const volumeIcon = document.querySelector('#volume-controls img');
+  const playButton = document.querySelector('button');
+  const jsConfetti = new JSConfetti();
+
+  hornSelect.addEventListener('change', () => {
+    const selected = hornSelect.value;
+    hornImage.src = `assets/images/${selected}.svg`;
+    hornAudio.src = `assets/audio/${selected}.mp3`;
+  });
+
+  volumeSlider.addEventListener('input', () => {
+    const volume = parseInt(volumeSlider.value);
+    hornAudio.volume = volume / 100;
+
+    if (volume === 0) {
+      volumeIcon.src = 'assets/icons/volume-level-0.svg';
+    } else if (volume < 33) {
+      volumeIcon.src = 'assets/icons/volume-level-1.svg';
+    } else if (volume < 67) {
+      volumeIcon.src = 'assets/icons/volume-level-2.svg';
+    } else {
+      volumeIcon.src = 'assets/icons/volume-level-3.svg';
+    }
+  });
+
+  playButton.addEventListener('click', () => {
+    hornAudio.play();
+    if (hornSelect.value === 'party-horn') {
+      jsConfetti.addConfetti();
+    }
+  });
 }
